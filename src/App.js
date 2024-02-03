136b140cea6c53edcb57e55bd445b50d138e2e50
JuliaJPereira <juliapereira.odc@gmail.com>
2024-02-03 18:05:10 -0300

    Aula 2.6 React-hello – Estado com useState

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
+import { useState } from 'react';
 import { Header } from './components/Header';
 import { Main } from './components/Main';
 // import { Test } from './components/Test';
 
 export default function App() {
-  console.log('Teste no console do navegador');
+  const [name, setName] = useState('Julia');
+
+  // uma função dentro de outra é chamada de closure no React
+  function handleNameChange(event) {
+    const newName = event.target.value;
+    setName(newName);
+  }
 
   return (
     // React só pode retornar um elemento, então usamos um fragmento para englobar os dois elementos que queremos retornar.
     <>
       <Header size="large">Componete Header - Projeto react-hello</Header>
       <Main>
-        <input className="border" type="text" />
+        <div className="flex flex-col my-4">
+          <label className="text-sm mb-1" htmlFor="inputName">
+            Digite seu nome:
+          </label>
+          <input
+            autoFocus
+            id="inputName"
+            className="border p-1"
+            type="text"
+            onChange={handleNameChange}
+          />
+        </div>
         <p>
           {/* usamos chaves para inserir variáveis no JSX */}
-          Seu nome é Julia, seu nome possui 5 letras e sua idade é 26 anos.
+          Seu nome é {name}, seu nome possui {name.length} letras e sua idade é
+          26 anos.
         </p>
       </Main>
     </>
